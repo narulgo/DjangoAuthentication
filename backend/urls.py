1ce273b0e4527bbc5a5d7b0af1dffdf95089b8f0
Narul <naruln9494@gmail.com>
2021-04-22 22:10:45 +0600

    first commit

---
+++ backend/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import registration_view, logout_view, protected
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', obtain_auth_token, name='login'),
+    path('register/', registration_view, name='register'),
+    path('protected/', protected, name='protected'),
+    path('logout/', logout_view, name='logout'),
+]
